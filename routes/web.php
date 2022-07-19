8dfaa98ffb88a8e39ad41046072ab2a6a9ce84b5
san4a66 <racing-taz@e1.ru>
2022-07-19 17:59:15 +0500

    admin lte

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/admin', \App\Http\Controllers\AdminController::class);
 
 Route::get('/', function () {
     return view('welcome');
