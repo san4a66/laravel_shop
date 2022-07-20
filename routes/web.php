d7f89fe118b15c44b66e34cf3c0ebc4bf212d897
san4a66 <racing-taz@e1.ru>
2022-07-20 15:07:54 +0500

    ADD main.blade.php

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/admin', \App\Http\Controllers\AdminController::class);
+Route::get('/', \App\Http\Controllers\Main\IndexController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
