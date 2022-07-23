4e7768cee65555befee230d4478e055bd20e5bdc
san4a66 <racing-taz@e1.ru>
2022-07-23 10:53:10 +0500

    ADD CRUD Categories

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -155,7 +155,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{route('category.index')}}" class="nav-link">
                             <i class="nav-icon fas fa-th-list"></i>
                             <p>
                                 Категории
